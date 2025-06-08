bd7e6c5c2a7b24b65afdef8832c7b1551e24c12e
Vitor Ivanoff <vitorivanoff969@gmail.com>
2025-06-08 17:05:41 -0300

    Separadas as classes na pasta Entidades e criação da classe Relatorio

--- atividade_Avaliativa/Program.cs
+++ atividade_Avaliativa/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using atividade_Avaliativa.Servicos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,21 +10,31 @@ namespace atividade_Avaliativa
     class Program
     {
         static void Main(string[] args)
-        {
-            Produto produto;
+        {            
+            Cliente cliente = new Cliente(1, "Gabriel", "exemple@exemple.com", "123.456.789-10");
+            
+
             try
             {
-                 produto = new Produto(1, "Lápis", 1.30, "Diversos");
+                Produto lapis = new Produto(1, "Lápis", 1.30, "Diversos");
+                Produto caneta = new Produto(2, "Caneta", 2.50, "Diversos");
+                Produto borracha = new Produto(3, "borracha", 3.00, "Diversos");
 
-                produto.setNome("caneta"); 
-               
+                ItemPedido itemLapis = new ItemPedido(lapis, 10);
+                ItemPedido itemCaneta = new ItemPedido(caneta, 2);
+                ItemPedido itemBorracha = new ItemPedido(borracha, 4);
+
+                Pedido pedido = new Pedido(1, cliente, new List<ItemPedido> { itemLapis, itemCaneta }, DateTime.Now);
+
+                Relatorio relatorio = new Relatorio(new List<Pedido> { pedido });
+
+                relatorio.ListarPedidos();
             }
             catch(Exception ex) 
             {
                 Console.WriteLine(ex.Message);
             }
-            
-            
+                        
         }
     }
 
---
+++ atividade_Avaliativa/Servicos/Relatorio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_Avaliativa.Servicos
+{
+    class Relatorio
+    {
+        private List<Pedido> pedidos;
+
+
+        public Relatorio(List<Pedido> pedidos)
+        {
+            this.pedidos = pedidos;
+        }
+
+        public void ListarPedidos()
+        {
+            foreach (Pedido pedido in this.pedidos)
+            {
+                Console.WriteLine(pedido.getId() + " | " + pedido.getData()+ "\n");
+                Console.WriteLine("\tID: "+ pedido.getCliente().getId() +  "\n\tCliente: " + pedido.getCliente().getNome() + "\n\tCPF: " + pedido.getCliente().getCpf() + "\n\tE-mail: " + pedido.getCliente().getEmail() + "\n");
+                Console.WriteLine("\tItens do Pedido: ");
+
+                foreach (ItemPedido itemPedido in pedido.getItensPedido())
+                {
+                    Console.WriteLine("\t\tID: " + itemPedido.getProduto().getId());
+                    Console.WriteLine("\t\tProduto: " + itemPedido.getProduto().getNome());
+                    Console.WriteLine("\t\tCategoria: " + itemPedido.getProduto().getCategoria());
+                    Console.WriteLine("\t\tQuantidade: " + itemPedido.getQuantidade());
+                    Console.WriteLine("\t\tPreço: R$" + itemPedido.getProduto().getPreco());
+                    Console.WriteLine("\t\tSub Total: R$" + itemPedido.ValorTotalItem());
+                    Console.WriteLine("\t----------------------------------");
+                }
+                Console.WriteLine("\n\tValor Total do Pedido: R$" +pedido.getValorTotal()); 
+
+            }
+        }
+    }
+}
