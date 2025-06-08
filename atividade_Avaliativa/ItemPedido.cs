ea4f6f9d26ea7d2073f84f59c2195f5b090fda24
Vitor Ivanoff <vitorivanoff969@gmail.com>
2025-06-08 16:17:29 -0300

    Criação da classe ItemPedido

---
+++ atividade_Avaliativa/ItemPedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade_Avaliativa
+{
+    class ItemPedido
+    {
+        private Produto produto;
+        private int quantidade;
+
+        public ItemPedido(Produto produto, int quantidade)
+        {
+            this.produto = produto;
+            this.quantidade = quantidade;
+        }
+
+        public double ValorTotalItem()
+        {
+            return produto.getPreco() * quantidade;
+        }
+
+        public Produto getProduto() {return this.produto;}
+        public void setProduto(Produto produto) {this.produto = produto;}
+
+        public int getQuantidade() {return this.quantidade;}
+        public void setQuantidade(int quantidade) { this.quantidade = quantidade;}
+    }
+}
